f6327a4402a1a9dd168d5762b433fbc8ef01921f
amirbarari <122981666+amirbarari@users.noreply.github.com>
2024-03-17 12:59:10 +0330

    give token from gotfather and create new bot

---
+++ bot.js
@@ -0,0 +1,5 @@
+//creating my first bot
+const Telegraf = require('telegraf'); //import the telegraf library(node.js framework)
+const bot = new Telegraf('7015864795:AAFJIwMGO1M59foSXIwmymjKUbLO64W_DKw');
+
+bot.launch();
