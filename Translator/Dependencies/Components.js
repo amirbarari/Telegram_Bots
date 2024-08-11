e95d7878d3510ed05999db583c32b0e2516f4827
amirmohammad.b.p <amirmohammadbarari415@gmail.com>
2024-08-11 18:07:41 +0330

    Add menu_keyboard component

---
+++ Translator/Dependencies/Components.js
@@ -0,0 +1,15 @@
+const homeInlineKeyboard = {
+    reply_markup: {
+        inline_keyboard: [
+            [
+                { text: "Google" },
+                { text: "Microsotf" }
+            ],
+            [
+                { text: "gelare" }
+            ]
+        ]
+    }
+};
+
+module.exports = { homeInlineKeyboard };
